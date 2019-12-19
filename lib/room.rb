75f41f1d9e12564961d753dd7856e45d567a0371
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-12-19 13:09:53 -0500

    room class

---
+++ lib/room.rb
@@ -0,0 +1,26 @@
+class Room
+  attr_accessor :n, :s, :e, :w, :name, :description
+  @@MAP = [4];
+
+  def self.create(name, n, s, e, w, description = nil)
+    r = self.new
+    r.name = name
+    r.n = n
+    r.s = s
+    r.e = e
+    r.w = w
+    r.description = description
+    self.map << r
+  end
+
+  def self.create_map
+    self.create("Troll Room", -1, 2, -1, 1)
+    self.create("Forest", -1, -1, 0, -1)
+    self.create("Cave", 0, -1, -1, 3)
+    self.create("Dungeon", -1, -1, 2, -1)
+  end
+
+  def self.map
+    @@MAP
+  end
+end
