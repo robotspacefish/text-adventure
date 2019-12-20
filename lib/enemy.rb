4d39572eea3cf7cac3f02343e123784c1762438e
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-12-20 10:03:21 -0500

    create goblin class method

--- lib/enemy.rb
+++ lib/enemy.rb
@@ -14,4 +14,14 @@ class Enemy < Actor
     super
     puts "The #{self.type} dropped the #{item}."
   end
+
+  def self.create_goblin(location)
+    e = self.new(location)
+    e.type = 'goblin'
+    e.description = 'Goblin' # todo
+    # todo possible inventory from randomized items (instances)
+
+    self.all << e
+
+  end
 end
